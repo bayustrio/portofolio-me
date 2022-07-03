59c14954afa13a562e9e94d3ea8b77f550835b9e
bayustrio7 <bayustrio7@gmail.com>
2022-07-04 05:41:13 +0700

    add SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../Layout/Layout";
 import ThemeProv from "../Context/ThemeContext";
+import { NextSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProv>
       <Layout>
+        <NextSeo title="Muhammad Satrio" description="Portofolio satrio" />
+
         <Component {...pageProps} />
       </Layout>
     </ThemeProv>
